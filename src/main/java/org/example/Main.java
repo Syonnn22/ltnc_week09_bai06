c0fa98ca2692639e7e49662aeb8cecdee57396e7
NguyenMinhThaiSon_25020351 <25020351@vnu.edu.vn>
2026-05-04 10:47:29 +0700

    ko cache

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        System.out.println("Hello and welcome!");
+        Student student = new Student("John Doe", 3.5);
+        System.out.println("Student Name: " + student.name);
+        System.out.println("Student GPA: " + student.gpa);
+    }
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Student {
+    String name;
+    double gpa;
+    Student(String name, double gpa){
+        this.gpa=gpa;
+        this.name=name;
+    }
+
+}
